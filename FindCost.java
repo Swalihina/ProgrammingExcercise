0b7d78673b8c1dfe596cee3c2abeb5eb3d0d6858
Swalihina <150607856+Swalihina@users.noreply.github.com>
2024-02-07 09:15:04 +0300

    Update FindCost.java

--- FindCost.java
+++ FindCost.java
@@ -1,13 +1,11 @@
-import java.util.Scanner; // import the Scanner class from the util package
-/* a program to input the initial price of a product and then calculate and display its cost after tax 
-has been added */
+import java.util.Scanner; 
 public class FindCost3
 { 
 public static void main(String[] args )
  { 
         Scanner keyboard = new Scanner(System.in); // create Scanner object 
         double price, tax;
-        System.out.println("*** Product Price Check ***");
+        System.out.println(" Product Price Check ");
         System.out.print("Enter initial price: "); // prompt for input 
         price = keyboard.nextDouble(); // input method called 
         System.out.print("Enter tax rate: "); // prompt for input 
